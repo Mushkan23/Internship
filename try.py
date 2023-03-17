6d3efde9d167d41847a0b9851f582a376c5ee31a
internship_feb <86152261+Mushkan23@users.noreply.github.com>
2023-03-17 10:38:15 +0530

    Add files via upload

--- try.py
+++ try.py
@@ -1,27 +1,58 @@
-#set = {'one','two','three'}
-'''empty_set = set
-print (empty_set)
+# Import module
+import sqlite3
 
-rate = [1,2,45,4]
-get = len(rate)
-print(get)
+# Connecting to sqlite
+conn = sqlite3.connect('gfg1.db')
 
-skills = {'problem solving','database','operating system'}
-skills.remove('database')
-print(skills)
+# Creating a cursor object using
+# the cursor() method
+cursor = conn.cursor()
 
-skills = {'problem','solving','databse'}
-if 'java' in skills : 
-    skills.remove('java')
-else:
-    skills.discard('java')
-'''
-    
-'''firstname = input("Enter your first name")
-lastname = input("Enter your last name")
-fullname = "the name is "{firstname}  {lastname}
-print(fullname)'''
+# Creating table
+table ="""CREATE TABLE EMPLOYEE(FIRST_NAME VARCHAR(255),
+LAST_NAME VARCHAR(255),AGE int, SEX VARCHAR(255), INCOME int);"""
+cursor.execute(table)
 
-count = 78
-print(id(count))
+# Queries to INSERT records.
+cursor.execute(
+	'''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)
+	VALUES ('Anand', 'Choubey', 25, 'M', 10000)''')
 
+cursor.execute(
+	'''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)
+	VALUES ('Mukesh', 'Sharma', 20, 'M', 9000)''')
+
+cursor.execute(
+	'''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)
+	VALUES ('Ankit', 'Pandey', 24, 'M', 6300)''')
+
+cursor.execute(
+	'''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)
+	VALUES ('Subhdra ', 'Singh', 26, 'F', 8000)''')
+
+cursor.execute(
+	'''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)
+	VALUES ('Tanu', 'Mishra', 24, 'F', 6500)''')
+
+# Display data inserted
+print("EMPLOYEE Table: ")
+data = cursor.execute('''SELECT * FROM EMPLOYEE''')
+for row in data:
+	print(row)
+
+# Updating
+cursor.execute('''UPDATE EMPLOYEE SET INCOME = 5000 WHERE Age<25;''')
+print('\nAfter Updating...\n')
+
+# Display data
+print("EMPLOYEE Table: ")
+data = cursor.execute('''SELECT * FROM EMPLOYEE''')
+for row in data:
+	print(row)
+
+
+# Commit your changes in the database
+conn.commit()
+
+# Closing the connection
+conn.close()
